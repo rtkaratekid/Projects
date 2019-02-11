a7e7b0d118614233ed72fa5ec53fff909ec5fca3
Tristan <mayfieldtristan@gmail.com>
2019-02-11 15:23:06 -0700

    fstrength update

--- FStrengthProg/main.cpp
+++ FStrengthProg/main.cpp
@@ -25,17 +25,7 @@ void writeAndPrintProgress(fstream &output,
 }
 
 int main(int argc, const char * argv[]) {
-    /*
-     clang++ -std=c++11 -c main.cpp
-     This compiles main.cpp into the file main.o (check this with ls to confirm).  The -std=c++11 part lets us use some things that C++ added in 2011.  The -c means "compile"
-     now, we're going to link the compiled files together (in this case there's actually only 1), again using clang:
-     clang++ -o main main.o
-     This will create a file called main which is the executable program.  The "-o main" part means "name the output main" Run your executable program with the following:
-     ./main
-     the ./ part just means "use the program called main in this folder"
-     Try passing commandline arguments to your program with something like:
-     ./main these are command line argument
-     */
+    
     double weight, reps;
     cout << "\nData for 10 second, two-arm hangs";
     cout << "\nTotal Weight: ";
